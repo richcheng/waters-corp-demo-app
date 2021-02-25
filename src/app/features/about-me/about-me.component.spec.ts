a397878c3cbc5e30172189f1cac7ba06fcae85fa
RCheng <RCheng@eatonvance.com>
2021-02-25 06:46:50 -0500

    upgrade to a11

--- src/app/features/about-me/about-me.component.spec.ts
+++ src/app/features/about-me/about-me.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('AboutMeComponent', () => {
   let component: AboutMeComponent;
   let fixture: ComponentFixture<AboutMeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AboutMeComponent ]
     })
